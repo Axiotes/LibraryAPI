05a3bd054e9c43c1f044a1839789eac6d84d74df
Arthur Axiotes <axiotesarthur@gmail.com>
2025-02-20 14:00:02 -0300

    feat: endpoint to create a new reader

--- src/reader/reader.controller.ts
+++ src/reader/reader.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ReaderService } from './reader.service';
+import { ReaderDto } from './dtos/reader.dto';
+import { Reader } from './reader.entity';
 
 @Controller('api/v1/reader')
 export class ReaderController {
-    constructor(private readonly readerService: ReaderService) {}
+  constructor(private readonly readerService: ReaderService) {}
+
+  @Post()
+  public async create(@Body() body: ReaderDto): Promise<Reader> {
+    return await this.readerService.create(body);
+  }
 }
--- src/reader/reader.service.ts
+++ src/reader/reader.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Reader } from './reader.entity';
 import { Repository } from 'typeorm';
+import { ReaderDto } from './dtos/reader.dto';
 
 @Injectable()
 export class ReaderService {
@@ -9,4 +10,27 @@ export class ReaderService {
     @InjectRepository(Reader)
     private readonly readerRepository: Repository<Reader>,
   ) {}
+
+  public async create(readerDto: ReaderDto): Promise<Reader> {
+    const readerEmail = await this.readerRepository.findOne({
+      where: { email: readerDto.email },
+    });
+
+    if (readerEmail) {
+      throw new ConflictException(
+        `O e-mail ${readerDto.email} já está cadastrado.`,
+      );
+    }
+
+    const readerCpf = await this.readerRepository.findOne({
+      where: { cpf: readerDto.cpf },
+    });
+
+    if (readerCpf) {
+      throw new ConflictException(`O CPF ${readerDto.cpf} já está cadastrado.`);
+    }
+
+    const reader = await this.readerRepository.create(readerDto);
+    return await this.readerRepository.save(reader);
+  }
 }
