9505c36f2b2c6e9d35f7c0b01ee739dc39891bba
Arthur Axiotes <axiotesarthur@gmail.com>
2025-02-20 23:39:02 -0300

    feat: endpoint to find books with title, author, genres or date

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -5,21 +5,29 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Query,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookDto } from './dtos/book-dto';
+import { Book } from './book.entity';
+import { FindBookDto } from './dtos/find-book.dto';
 
 @Controller('api/v1/book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Post()
-  public async create(@Body() body: BookDto) {
+  public async create(@Body() body: BookDto): Promise<Book> {
     return await this.bookService.create(body);
   }
 
   @Get(':id')
-  public findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.bookService.findOne(id);
+  public async findOne(@Param('id', ParseIntPipe) id: number): Promise<Book> {
+    return await this.bookService.findOne(id);
+  }
+
+  @Get()
+  public async find(@Query() query: FindBookDto) {
+    return await this.bookService.find(query);
   }
 }
