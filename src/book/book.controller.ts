fc51b4d878c7a8ee19294d54e0290bcaab45db65
Arthur Axiotes <axiotesarthur@gmail.com>
2025-02-20 21:52:45 -0300

    feat: endpoint to create a new book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { BookService } from './book.service';
+import { BookDto } from './dtos/book-dto';
 
-@Controller('book')
+@Controller('api/v1/book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  public async create(@Body() body: BookDto) {
+    return await this.bookService.create(body);
+  }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { Repository } from 'typeorm';
+import { BookDto } from './dtos/book-dto';
 
 @Injectable()
 export class BookService {
   constructor(
     @InjectRepository(Book) private readonly bookRepository: Repository<Book>,
   ) {}
+
+  public async create(bookDto: BookDto): Promise<Book> {
+    const book = await this.bookRepository.create(bookDto);
+    return await this.bookRepository.save(book);
+  }
 }
