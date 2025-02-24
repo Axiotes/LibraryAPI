d0c17ce77061cc3c0a822de5bdee44b6dcabdbb5
Arthur Axiotes <axiotesarthur@gmail.com>
2025-02-24 00:26:40 -0300

    feat: add guard to the books endpoints

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -8,17 +8,23 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookDto } from './dtos/book-dto';
 import { Book } from './book.entity';
 import { FindBookDto } from './dtos/find-book.dto';
 import { UpdateBookDto } from './dtos/update-book.dto';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { AuthGuard } from '@nestjs/passport';
+import { RoleGuard } from 'src/auth/guards/role/role.guard';
 
 @Controller('api/v1/book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
+  @UseGuards(AuthGuard('jwt'), RoleGuard)
+  @Roles('admin', 'employee')
   @Post()
   public async create(@Body() body: BookDto): Promise<Book> {
     return await this.bookService.create(body);
@@ -34,6 +40,8 @@ export class BookController {
     return await this.bookService.find(query);
   }
 
+  @UseGuards(AuthGuard('jwt'), RoleGuard)
+  @Roles('admin', 'employee')
   @Patch(':id')
   public async update(
     @Param('id', ParseIntPipe) id: number,
@@ -42,8 +50,10 @@ export class BookController {
     return await this.bookService.update(id, body);
   }
 
+  @UseGuards(AuthGuard('jwt'), RoleGuard)
+  @Roles('admin')
   @Delete(':id')
-    public async delete(@Param('id', ParseIntPipe) id: number) {
-      return await this.bookService.delete(id);
-    }
+  public async delete(@Param('id', ParseIntPipe) id: number) {
+    return await this.bookService.delete(id);
+  }
 }
