052f146a8f60e38503e3b0a210d65f6b2495b394
Arthur Axiotes <axiotesarthur@gmail.com>
2025-02-24 19:52:33 -0300

    feat: seeder for fake auth users

--- src/db/seeders/main-seeder.ts
+++ src/db/seeders/main-seeder.ts
@@ -3,16 +3,19 @@ import { runSeeder, Seeder, SeederFactoryManager } from 'typeorm-extension';
 import ReadersSeeder from './readers.seed';
 import BooksSeeder from './books.seed';
 import LoansSeerder from './loans.seeder';
+import FakeAuthSeeders from './fake-auth.seeder';
 
 export class MainSeeder implements Seeder {
   public async run(
     dataSource: DataSource,
     factoryManager: SeederFactoryManager,
   ): Promise<void> {
-    // await runSeeder(dataSource, ReadersSeeder);
-    // await runSeeder(dataSource, BooksSeeder);
+    await runSeeder(dataSource, ReadersSeeder);
+    await runSeeder(dataSource, BooksSeeder);
     await runSeeder(dataSource, LoansSeerder);
+    await runSeeder(dataSource, FakeAuthSeeders);
 
-    console.log(process.argv); // 5 to admin flag
+    console.log("process argv: ", process.argv); // 5 to admin flag
+    console.log("flag: ", process.argv[5]);
   }
 }
