b127aed9a7015d8ff744646a575d99649a4a2536
Arthur Axiotes <axiotesarthur@gmail.com>
2025-02-24 11:06:33 -0300

    feat: add swagger documentation to the auth module

--- src/auth/dtos/sign-in.dto.ts
+++ src/auth/dtos/sign-in.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString } from "class-validator";
 
 export class SignInDto {
+  @ApiProperty({ description: "Email do usuário autenticado" })  
   @IsEmail()
   email: string;
 
+  @ApiProperty({ description: "Senha do usuário autenticado" })  
   @IsString()
   password: string;
 }
--- src/auth/dtos/update-auth.dto.ts
+++ src/auth/dtos/update-auth.dto.ts
@@ -1,20 +1,26 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class UpdateAuthDto {
+  @ApiProperty({ description: "Email do usuário autenticado" })  
   @IsEmail()
   email: string;
 
+  @ApiProperty({ description: "Senha do usuário autenticado" })  
   @IsString()
   password: string;
 
+  @ApiPropertyOptional({ description: "Novo email do usuário autenticado" })  
   @IsOptional()
   @IsEmail()
   newEmail: string;
 
+  @ApiPropertyOptional({ description: "Nova senha do usuário autenticado" })  
   @IsOptional()
   @IsString()
   newPassword: string;
 
+  @ApiPropertyOptional({ description: "Novo nome do usuário autenticado" })  
   @IsOptional()
   @IsString()
   name: string;
