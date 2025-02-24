caa92bdca8e475d732e63b7f7ea0df36ad47d601
Arthur Axiotes <axiotesarthur@gmail.com>
2025-02-23 23:18:34 -0300

    feat: create jwt token when creating a new auth user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, ClassSerializerInterceptor, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dtos/auth.dto';
+import { Auth } from './auth.entity';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('api/v1/auth')
@@ -8,7 +9,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  public async create(@Body() body: AuthDto) {
+  public async create(@Body() body: AuthDto): Promise<{ token: string, authUser: Auth }> {
     return await this.authService.create(body);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Auth } from './auth.entity';
 import { Repository } from 'typeorm';
 import { AuthDto } from './dtos/auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(Auth) private readonly authRepository: Repository<Auth>,
+    private jwtService: JwtService,
   ) {}
 
-  public async create(authDto: AuthDto): Promise<Auth> {
+  public async create(
+    authDto: AuthDto,
+  ): Promise<{ token: string; authUser: Auth }> {
     const authUserEmail = await this.authRepository.findOne({
       where: { email: authDto.email },
     });
@@ -22,6 +26,13 @@ export class AuthService {
     }
 
     const authUser = await this.authRepository.create(authDto);
-    return await this.authRepository.save(authUser);
+    const savedAuthUser = await this.authRepository.save(authUser);
+
+    const token = this.jwtService.sign({
+      id: savedAuthUser.id,
+      role: savedAuthUser.role,
+    });
+
+    return { token, authUser: savedAuthUser };
   }
 }
