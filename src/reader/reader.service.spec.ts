fb9390ec2abf083f9fa327d558434456c83b1214
Arthur Axiotes <axiotesarthur@gmail.com>
2025-03-17 13:42:31 -0300

    feat: teste para criacao de um novo leitor

--- src/reader/reader.service.spec.ts
+++ src/reader/reader.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReaderService } from './reader.service';
+import { Reader } from './reader.entity';
+import { Repository } from 'typeorm';
+import { ReaderDto } from './dtos/reader.dto';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ReaderService', () => {
   let service: ReaderService;
+  let readerRepositoryMock: Partial<jest.Mocked<Repository<Reader>>>;
 
   beforeEach(async () => {
+    readerRepositoryMock = {
+      findOne: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReaderService],
+      providers: [
+        ReaderService,
+        { provide: getRepositoryToken(Reader), useValue: readerRepositoryMock },
+      ],
     }).compile();
 
     service = module.get<ReaderService>(ReaderService);
+    jest.clearAllMocks();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should create a new user successfuly', async () => {
+    const readerDto: ReaderDto = {
+      name: 'Test Reader',
+      email: 'test@example.com',
+      cpf: '12345678901',
+    };
+
+    readerRepositoryMock.findOne.mockResolvedValueOnce(null);
+    const reader = readerRepositoryMock.create(readerDto);
+    readerRepositoryMock.save.mockResolvedValueOnce(reader);
+
+    const result = await service.create(readerDto);
+
+    expect(readerRepositoryMock.findOne).toHaveBeenCalledTimes(2);
+    expect(readerRepositoryMock.create).toHaveBeenCalledWith(readerDto);
+    expect(readerRepositoryMock.save).toHaveBeenCalledWith(reader);
+    expect(result).toEqual(reader);
   });
 });
