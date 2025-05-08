1d28f1834bac374529b6b77859e2172e9cf2dd81
Arthur Axiotes <axiotesarthur@gmail.com>
2025-05-08 09:48:42 -0300

    feat: role guard tests

--- src/common/guards/role/role.guard.spec.ts
+++ src/common/guards/role/role.guard.spec.ts
@@ -1,7 +1,65 @@
-import { JwtRoleGuard } from './role.guard';
+import { Reflector } from '@nestjs/core';
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
 
-describe('JwtRoleGuard', () => {
+import { RoleGuard } from './role.guard';
+
+describe('RoleGuard', () => {
+  let guard: RoleGuard;
+  let reflector: Reflector;
+
+  beforeEach(() => {
+    reflector = new Reflector();
+    guard = new RoleGuard(reflector);
+  });
+
+  const mockExecutionContext = (
+    userRole?: string,
+    _roles?: string[],
+  ): ExecutionContext =>
+    ({
+      switchToHttp: () => ({
+        getRequest: (): { user?: { role?: string } } => ({
+          user: userRole ? { role: userRole } : undefined,
+        }),
+      }),
+      getHandler: jest.fn(),
+    }) as unknown as ExecutionContext;
+
   it('should be defined', () => {
-    expect(new JwtRoleGuard()).toBeDefined();
+    expect(guard).toBeDefined();
+  });
+
+  it('should allow access if user has required role', () => {
+    const context = mockExecutionContext('admin', ['admin']);
+    jest.spyOn(reflector, 'get').mockReturnValue(['admin']);
+
+    expect(guard.canActivate(context)).toBe(true);
+  });
+
+  it('should allow access if no roles are required', () => {
+    const context = mockExecutionContext('user');
+    jest.spyOn(reflector, 'get').mockReturnValue(undefined);
+
+    expect(guard.canActivate(context)).toBe(true);
+  });
+
+  it('should throw UnauthorizedException if user is not authenticated', () => {
+    const context = mockExecutionContext(undefined);
+    jest.spyOn(reflector, 'get').mockReturnValue(['admin']);
+
+    expect(() => guard.canActivate(context)).toThrow(
+      new UnauthorizedException('Token JWT não encontrado ou inválido'),
+    );
+  });
+
+  it('should throw UnauthorizedException if user role is not allowed', () => {
+    const context = mockExecutionContext('user', ['admin']);
+    jest.spyOn(reflector, 'get').mockReturnValue(['admin']);
+
+    expect(() => guard.canActivate(context)).toThrow(
+      new UnauthorizedException(
+        'Você não tem permissão para acessar essa rota',
+      ),
+    );
   });
 });
